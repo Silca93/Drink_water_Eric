5764d9c320d3891f890b1e6c6ec3d65fad28e572
Eric_PC_FIXE <edelhaye93@gmail.com>
2024-01-21 21:18:11 +0100

    it finally starts working a bit

--- public/js/main.js
+++ public/js/main.js
@@ -13,3 +13,47 @@ glass1.addEventListener('click', () => {
 
 
 })
+
+let glass2 = document.querySelector(".dos");
+
+let secondDrinkbg = document.querySelector(".secondDrinkbg");
+let secondDrink = document.querySelector(".secondDrink")
+glass2.addEventListener('click', () => {
+    bottleText.innerText = "1.5 L Remain";
+    secondDrink.style.color = "#0f46bb"
+    // firstDrinkbg.style.height = "88px"
+    // firstDrink.innerText = "25%"
+    // firstDrinkbg.classList.toggle('expand');
+    secondDrinkbg.classList.toggle('expand');
+        
+});
+
+let glass3 = document.querySelector(".tres");
+
+let thirdDrinkbg = document.querySelector(".thirdDrinkbg");
+let thirdDrink = document.querySelector(".thirdDrink")
+glass3.addEventListener('click', () => {
+    bottleText.innerText = "1.25 L Remain";
+    thirdDrink.style.color = "#0f46bb"
+    // firstDrinkbg.style.height = "88px"
+    // firstDrink.innerText = "25%"
+    // firstDrinkbg.classList.toggle('expand');
+    thirdDrinkbg.classList.toggle('expand');
+        
+});
+
+// let glass3 = document.querySelector(".tres");
+// let thirdDrink = document.querySelector(".thirdDrink");
+
+// glass3.addEventListener('click', () => {
+//     bottleText.innerText = "1.25 L Remain";
+//     firstDrinkbg.style.height = "132px"
+//     firstDrink.innerText = "37.5%"
+// })
+
+// let glass4 = document.querySelector(".quatro")
+// glass4.addEventListener('click', () => {
+//     bottleText.innerText = "1 L Remain";
+//     firstDrinkbg.style.height = "176px"
+//     firstDrink.innerText = "50%"
+// })
